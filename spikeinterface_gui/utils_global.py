05dce6db0d2e6a78558ed7aef99d3b91c08a56ea
chrishalcrow <chrishalcrow@gmail.com>
2025-10-27 11:56:17 +0000

    clean utils global

--- spikeinterface_gui/utils_global.py
+++ spikeinterface_gui/utils_global.py
@@ -2,49 +2,15 @@ import numpy as np
 from pathlib import Path
 import os
 
-def get_size_top_row(initial_row, initial_col, is_zone_array, original_zone_array):
-    
-    if original_zone_array[initial_row][initial_col] == False:
-        return 0,0
+def get_config_folder() -> Path:
+    """Get the config folder for spikeinterface-gui settings files.
 
-    num_rows = is_zone_array[initial_row][initial_col]*1
-    num_cols = num_rows
-
-    num_rows += (not is_zone_array[1][initial_col])*1
-
-    if num_rows == 1:
-        for zone in is_zone_array[0,1+initial_col:]:
-            if zone == True:
-                break
-            num_cols += 1
-    elif num_rows == 2:
-        for zone1, zone2 in np.transpose(is_zone_array[:,1+initial_col:]):
-            if zone1 == True or zone2 == True:
-                break
-            num_cols += 1
-
-    is_zone_array[initial_row:initial_row+num_rows,initial_col:initial_col+num_cols] = True
-
-    return num_rows, num_cols
-
-def get_size_bottom_row(initial_row, initial_col, is_zone_array, original_zone_array):
-    
-    if original_zone_array[initial_row][initial_col] == False:
-        return 0,0
-    
-    num_rows = is_zone_array[initial_row][initial_col]*1
-    if num_rows == 0:
-        return 0, 0
-    num_cols = num_rows
-
-    for zone in is_zone_array[1,1+initial_col:]:
-        if zone == True:
-            break
-        else:
-            num_cols += 1
-
-    return num_rows, num_cols
-
+    Returns
+    -------
+    cache_folder : Path
+        The path to the cache folder.
+    """
+    return Path(os.path.expanduser("~")) / ".config" / "spikeinterface_gui"
 
 # Functions for the layout
 
@@ -92,15 +58,3 @@ def get_present_zones_in_half_of_layout(layout_zone, shift):
     is_present = [views is not None and len(views) > 0 for views in half_dict.values()]
     present_zones = set(np.array(list(half_dict.keys()))[np.array(is_present)])
     return present_zones
-
-  
- 
-def get_config_folder() -> Path:
-    """Get the config folder for spikeinterface-gui settings files.
-
-    Returns
-    -------
-    cache_folder : Path
-        The path to the cache folder.
-    """
-    return Path(os.path.expanduser("~")) / ".config" / "spikeinterface_gui"
